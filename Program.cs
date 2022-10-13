444014d9f4827660e92538112685194b33d205ed
Grigory Teleshev <Grigory.Teleshev@yandex.ru>
2022-10-13 22:51:39 +0300

    fourth commit

--- Program.cs
+++ Program.cs
@@ -6,44 +6,26 @@
 
 Console.Clear();
 
-int[] paramArray = InputCheck();
-int rows = paramArray[0];
-int columns = paramArray[1];
-int min = paramArray[2];
-int max = paramArray[3];
-int decimals = paramArray[4];
+string[] stringArrayToProcess = InputCheck();
 
-int[,] array = GetArray(rows, columns, min, max);
+PrintArray(stringArrayToProcess);
+string[] outStringArray = OutPutArray(stringArrayToProcess);
 Console.WriteLine();
-Console.WriteLine("Массив:");
-PrintArray(array);
-Console.WriteLine("Среднее по столбцам:");
+PrintArray(outStringArray);
 
-Console.WriteLine(String.Join(" ", ColumnAverages(array, decimals)));
-
-int[] InputCheck()
+string[] InputCheck()
 {
     Console.Clear();
-    int[] parametersArray = new int[5];
-    Console.WriteLine("Введите параметры двумерного массива:");
-    
-    bool res1 = false;
-    int M = parametersArray.Length;
-    string errorMessage = "Число введено неверно.";
-    string secondMessage = "-";
-    string[] prefixes = new string [5];
-    prefixes[0] = "количество строк массива";
-    prefixes[1] = "количество столбцов массива";
-    prefixes[2] = "минимум";
-    prefixes[3] = "максимум";
-    prefixes[4] = "количество знаков для расчёта среднего";
-    
     int count = 0;
-    while (!res1 || count < M)
+    string firstMessage = "Введите количество строк массива: ";
+    string errorMessage = "Число введено неверно.";
+    bool res = false;
+    int M = 1;
+    while (!res)
     {
-        Console.Write($"{secondMessage} {prefixes[count]}: ");
-        res1 = int.TryParse(Console.ReadLine(), out parametersArray[count]);
-        if (!res1 == true)
+        Console.Write($"{firstMessage}: ");
+        res = int.TryParse(Console.ReadLine(), out M);
+        if (!res == true)
         {
             Console.WriteLine(errorMessage);
         }
@@ -52,59 +34,43 @@ int[] InputCheck()
             count++;
         }
     }
-    return parametersArray;
+
+    string[] stringArray = new string[M];
+
+    string secondMessage = "элемент ";
+
+    int count1 = 0;
+    bool res1 = false;
+    while (count1 < M)
+    {
+        Console.Write($"{secondMessage} {count1}: ");
+        stringArray[count1] = Console.ReadLine();
+        count1++;
+    }
+    return stringArray;
 }
 
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+string[] OutPutArray(string[] stringArray)
 {
-    int interimVar = 0;
-    if(minValue > maxValue)
-    {
-        interimVar = maxValue;
-        maxValue = minValue;
-        minValue = interimVar;
-    }
-    
-    int[,] result = new int[m, n];
+    int m = stringArray.Length;
+    int stringSize = 0;
+    string[] outputArray = new string[stringSize];
     for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < n; j++)
+        if (stringArray[i].Length <= 3)
         {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            stringSize++;
+            Array.Resize(ref outputArray, stringSize);
+            outputArray[stringSize - 1] = stringArray[i];
         }
     }
-    return result;
-}
-double[] ColumnAverages(int[,] intArray, int numberOfDecimals)
-{
-    int m = intArray.GetLength(0);
-    int n = intArray.GetLength(1);
-    double[] averagesArray = new double[n];
-    double sum = 0;
-    for (int j = 0; j < n; j++)
-    {
-        sum = 0;
-        for (int i = 0; i < m; i++)
-        {
-            sum = sum + intArray[i, j];
-            
-            if (i == m - 1) 
-            {
-                averagesArray[j] = Math.Round(sum / m, numberOfDecimals);
-            }
-        }
-    }
-    return averagesArray;
+    return outputArray;
 }
 
-void PrintArray(int[,] inArray)
+void PrintArray(string[] inArray)
 {
-    for (int i = 0; i < inArray.GetLength(0); i++)
+    for (int i = 0; i < inArray.Length; i++)
     {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            Console.Write($"{inArray[i, j]} ");
-        }
-        Console.WriteLine();
+        Console.Write($"{inArray[i]} ");
     }
 }
